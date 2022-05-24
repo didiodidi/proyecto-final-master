df21faece91d3adc218d360bfd288a59baa45da9
callegarifede1 <callegarifede1@gmail.com>
2022-05-24 02:28:33 -0300

    Creacion del modal ,steps

--- carritoIndex.js
+++ carritoIndex.js
@@ -1,157 +1,160 @@
-import { actualizarCarrito} from "./actualizarCarrito.js";
+import { actualizarCarrito } from "./actualizarCarrito.js";
 import { productos } from "./stock.js";
 
-
-
-
-
-const contenedorCarrito = document.getElementById('carrito-contenedor');
-
-
+const contenedorCarrito = document.getElementById("carrito-contenedor");
+const cantidadSteps = 2;
+let stepActual = 1;
 // --->Funcion comprar , ejecuta el boton comprar del modal:
-const botonVaciar = document.getElementById('comprar')
+const botonVaciar = document.getElementById("comprar");
 
 let carritoDeCompras = [];
 
-botonVaciar.addEventListener('click',()=>{
+const botonContinuar = document.getElementById("continuar");
 
+const terminarCompra = document.getElementById("terminar-compra");
 
-    if(carritoDeCompras <=0 ){
-      
-      Swal.fire({
-        icon: 'error',
-        text: 'Ingrese un producto en el carrito',
-      })}else{
-      const arrayCompras = carritoDeCompras
-      arrayCompras.length = 0;
-      
-    //--
-  
+terminarCompra.addEventListener("click", (e) => {
+  console.log("terminar compra");
+  stepActual = 1;
+  location.reload();
+});
 
-    Swal.fire({
-      icon: 'question',
-      title: 'Usted desea realizar la compra?',
-      showDenyButton: true,
-      confirmButtonText: 'Aceptar',
-      denyButtonText: `Cancelar`,
-    }).then((result) => {
-      /* Read more about isConfirmed, isDenied below */
-      if (result.isConfirmed) {
-        //Swal.fire('Usted realizo la compra con exito!', '', 'success')
-
-        let timerInterval
-        Swal.fire({
-      title: 'La compra esta siendo procesada',
-      html: 'Ejecutando por favor espere... <b></b> milliseconds.',
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: () => {
-        Swal.showLoading()
-        const b = Swal.getHtmlContainer().querySelector('b')
-        timerInterval = setInterval(() => {
-        b.textContent = Swal.getTimerLeft()
-    }, 100)
-  },
-    willClose: () => {
-    clearInterval(timerInterval)
-    }
-      }).then((result) => {
-  
-    if (result.dismiss === Swal.DismissReason.timer) {
-    console.log('I was closed by the timer')
-    }
-    }) 
-
-
-
-      } else if (result.isDenied) {
-        Swal.fire('Changes are not saved', '', 'info')
-      }
-    })
-    actualizarCarrito(arrayCompras);
-    vaciarDom();
-  }
-    
-
-    //--
-    
-    //Forma alternativa de resetear el productoEnCarrito del DOMContentLoaded--->>
-    // location.reload();
-})
-//Borre el productoEnCarrito del DOM con la siguente funcion ,de la linea 15 del archivo index.js.Este persistia en el DOM por el DOMContentLoaded  --->>
-function vaciarDom () {
-  let borrar = document.querySelectorAll('.productoEnCarrito');
-  borrar.forEach(element => element.remove());
+function show(elem) {
+  elem.classList.remove("hidden");
 }
 
+function hide(elem) {
+  elem.classList.add("hidden");
+}
+
+function irAlStep(stepNumber) {
+  stepActual = stepNumber;
+
+  let stepsAEsconder = document.getElementsByClassName("modal-carrito");
+  let steps = document.getElementsByClassName(`step${stepActual}`);
+
+  //Esconder los steps
+  for (let i = 0; i < stepsAEsconder.length; ++i) {
+    hide(stepsAEsconder[i]);
+  }
+
+  //only show the right one
+  for (let i = 0; i < steps.length; ++i) {
+    show(steps[i]);
+  }
+
+  // //Si alcanzamos el ultimo step
+  // if (stepActual === cantidadSteps) {
+  //   disable(botonContinuar);
+  //   // show(submitButton);
+  // }
+
+  // //else if first step
+  // else if (stepActual === 1) {
+  //   enable(botonContinuar);
+  //   // hide(submitButton);
+  // } else {
+  //   enable(botonContinuar);
+  //   // hide(submitButton);
+  // }
+}
+
+function continuarAlSiguienteStep(e) {
+  stepActual += 1;
+  irAlStep(stepActual);
+}
+
+botonContinuar.addEventListener("click", (e) => {
+  if (carritoDeCompras <= 0) {
+    Swal.fire({
+      icon: "error",
+      text: "Ingrese un producto en el carrito",
+    });
+  } else {
+    continuarAlSiguienteStep(e);
+    //Intentar crear el metodo de pago en otro modal.
+
+    // const modalComprar = document.querySelector('.modal-continuar');
+    //       modalComprar.document.createElement("div")
+    //       modalComprar.innerHTML = `<p>Hola</p>`
+
+    //Poner El metodo de pago debajo del modal
+    const modalComprar = document.getElementById("metodoDePago");
+    modalComprar.innerHTML = `<h2>HOLA</h2>`;
+  }
+});
+
+//Borre el productoEnCarrito del DOM con la siguente funcion ,de la linea 15 del archivo index.js.Este persistia en el DOM por el DOMContentLoaded  --->>
+function vaciarDom() {
+  let borrar = document.querySelectorAll(".productoEnCarrito");
+  borrar.forEach((element) => element.remove());
+}
 
 export const carritoIndex = (productoId) => {
-    if(localStorage.getItem("carrito")) {
-        carritoDeCompras = JSON.parse(localStorage.getItem("carrito"));
-    }
-    console.log(carritoDeCompras)
-    let productoRepetido = carritoDeCompras.find (producto => producto.id == productoId);
-    contarProductosRepetidos(productoRepetido, productoId);
-    eliminarProductoCarrito(productoId);
-}
-
+  if (localStorage.getItem("carrito")) {
+    carritoDeCompras = JSON.parse(localStorage.getItem("carrito"));
+  }
+  console.log(carritoDeCompras);
+  let productoRepetido = carritoDeCompras.find(
+    (producto) => producto.id == productoId
+  );
+  contarProductosRepetidos(productoRepetido, productoId);
+  eliminarProductoCarrito(productoId);
+};
 
 //------------->Eliminar Producto<----------------//
-export const eliminarProductoCarrito = (productoId, productoNombre ) => {
+export const eliminarProductoCarrito = (productoId, productoNombre) => {
   console.log(productoNombre);
-  if (localStorage.getItem("carrito")){
+  if (localStorage.getItem("carrito")) {
     carritoDeCompras = JSON.parse(localStorage.getItem("carrito"));
   }
   let botonEliminar = document.getElementById(`eliminar${productoId}`);
 
-  botonEliminar?.addEventListener('click', () => {
-    swal.fire({
-      title:`Se elimino el producto con exito`,
-      icon: 'success',
-      buttons: true ,
-      dangerMode: true
-    }).then((result) => {
-      if (result) {
-        botonEliminar.parentElement.remove();
-        carritoDeCompras = carritoDeCompras.filter(el => el.id != productoId);
-        actualizarCarrito(carritoDeCompras);
-      }
-    })
-  })   
-}
+  botonEliminar?.addEventListener("click", () => {
+    swal
+      .fire({
+        title: `Se elimino el producto con exito`,
+        icon: "success",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((result) => {
+        if (result) {
+          botonEliminar.parentElement.remove();
+          carritoDeCompras = carritoDeCompras.filter(
+            (el) => el.id != productoId
+          );
+          actualizarCarrito(carritoDeCompras);
+        }
+      });
+  });
+};
 
 //-----> COMPRAR PRODUCTO --->
 
-
-
 export const contarProductosRepetidos = (prodRepetido, productoId) => {
-  if (prodRepetido){
-    prodRepetido.cantidad++
-    document.getElementById(`cantidad${prodRepetido.id}`).innerHTML = `<p id=cantidad${prodRepetido.id}>Cantidad:${prodRepetido.cantidad}</p>`;
+  if (prodRepetido) {
+    prodRepetido.cantidad++;
+    document.getElementById(
+      `cantidad${prodRepetido.id}`
+    ).innerHTML = `<p id=cantidad${prodRepetido.id}>Cantidad:${prodRepetido.cantidad}</p>`;
     actualizarCarrito(carritoDeCompras);
-  }else{
+  } else {
     renderProductoCarrito(productoId);
   }
-}
+};
 
-const renderProductoCarrito =(productoId) => {
-  let producto =productos.find(producto => producto.id == productoId);
+const renderProductoCarrito = (productoId) => {
+  let producto = productos.find((producto) => producto.id == productoId);
   carritoDeCompras.push(producto);
   producto.cantidad = 1;
-  let div = document.createElement('div');
-  div.classList.add('productoEnCarrito');
+  let div = document.createElement("div");
+  div.classList.add("productoEnCarrito");
   div.innerHTML = ` <p>${producto.nombre}</p>
                     <p>Precio:${producto.precio}</p>
                     <p id=cantidad${producto.id}>Cantidad:${producto.cantidad}</p>
                     <button id="eliminar${producto.id}" class="boton-eliminar"><i class="fa-solid fa-trash-can"></i></button>
-                  `
-contenedorCarrito.appendChild(div);
-actualizarCarrito(carritoDeCompras);
-}
-
-
-
-
-
-
-
+                  `;
+  contenedorCarrito.appendChild(div);
+  actualizarCarrito(carritoDeCompras);
+};
