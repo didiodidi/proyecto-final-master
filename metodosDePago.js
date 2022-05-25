aaa676bd2a95253f87c2ba2813645186e9ad970a
callegarifede1 <callegarifede1@gmail.com>
2022-05-24 22:20:03 -0300

    modal compra

--- metodosDePago.js
+++ metodosDePago.js
@@ -1,22 +1,73 @@
+//import { productos } from "./stock.js";
 
+const localStorageCarrito = JSON.parse(localStorage.getItem("carrito"));
+
+            const precioTotalCarrito = localStorageCarrito.reduce(
+                (acc, el) => acc + el.precio,
+                0
+            );
+
+            function limpiarMetodoDePago (){
+                const divALimpiar = document.querySelector('.limpiar');
+                divALimpiar && divALimpiar.remove()
+                //divALimpiar?.remove();
+            }
 
 // Controlo los botones para poder seleccionar el metodo de compra.
-
+    const metodoEfectivo = document.getElementById("metodoEfectivo");
+    const metodoTarjeta = document.getElementById("metodoTarjeta");
     const checked1 = document.getElementById("efectivo");
     const checked2 = document.getElementById("tarjeta");
         checked1.addEventListener("click", respuestaClick1)
         function respuestaClick1(){
+            limpiarMetodoDePago();
             if(checked1.checked != false){
                 checked2.checked = null;
+                const div = document.createElement("div");
+            //Operador ++
+            div.innerHTML +=`<div class="limpiar" style="width: 18rem;">
+                                    <div>1 pago de ${precioTotalCarrito}</div>
+                            </div>`;
+            metodoEfectivo.appendChild(div);
+            
             }
         }
-    
         checked2.addEventListener("click", respuestaClick2)
             function respuestaClick2(){
+                limpiarMetodoDePago()
                 if(checked2.checked != false){
                     checked1.checked = null;
+
+                    const div = document.createElement("div");
+                //Operador ++
+                div.innerHTML +=`<div class="limpiar" style="width: 18rem;">
+                                    <div>
+                                        <input type="radio" name="Tarjeta" id="3pagos" value="2">
+                                        <label for="3pagos">   3 pagos de ${precioTotalCarrito / 3}  </label>
+                                    </div>
+                                    <div>
+                                        <input type="radio" name="Tarjeta" id="6pagos" value="2">
+                                        <label for="6pagos">   6 pagos de ${precioTotalCarrito / 6}  </label>
+                                    </div>
+                                    <div>
+                                        <input type="radio" name="Tarjeta" id="12pagos" value="2">
+                                        <label for="12pagos">   12 pagos de ${precioTotalCarrito / 12}  </label>
+                                    </div>
+                                    
+                                    
+
+
+                                </div>`;
+                metodoTarjeta.appendChild(div);
+
+
                 }
             } 
+
+            
+            
+
+
 
 //------
 
